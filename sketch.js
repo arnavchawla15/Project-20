2a949ae3d79d1c763814959cad0af2b604daabb8
arnavchawla15 <81763124+arnavchawla15@users.noreply.github.com>
2021-04-21 22:02:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,46 +1,60 @@
-var bullet,wall;
-var speed,weight;
-var thickness;
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var gameState = "play";
+var carAnimation, logAnimation, playerAnimation;
+var school;
+function preload()
+{
  
+}
 
 function setup() {
-  createCanvas(1600,400);
+  createCanvas(1366,2700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
 
-  speed = random(55,90);
-  weight = random(400,1500)
-  thickness = random(22,83)
+  for(var i=0;i<6;i++) {
+    var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
+    bottomGrass1.shapeColor = "green";
+    //adding road
+    if(i%2 == 0){
+      var road = createSprite(683,height-50-(i*400)-grassHeight,width,300)
+      road.shapeColor = "black";
+    }
+  }
+   
+  // to create rows of cars:
+  for(var i = 0; i < 4; i++){
+    cars = new Car(2);
+    carGroup1.add(cars.spt)
+  }
+  // to create rows of logs:
+  for(var i = 0; i < 4; i++){
+    logs = new Log(2);
+    logGroup1.add(logs.spt)
+  }
 
-  bullet = createSprite(50,200,50,50);
-  bullet.velocityX = speed;
-  bullet.shapeColor = "white";
 
-  wall = createSprite(1500,200,thickness,height/2);
-  wall.shapeColor = color(80,80,80);
+  for(i == 1;i<logGroup1.length;i++){
+    if(logGroup1[i].x<0){
+      logGroup1[i].x = width;
+    }
+  }
 
-}
+  for(i == 1;i<carGroup1.length;i++){
+    if(carGroup1[i].x<0 || carGroup1[i].x>1366){
+      carGroup1[i].x = width;
+    }
+  }
+ }
 
 function draw() {
-  background("black");  
-
-  bullet.collide(wall);
-
-if(wall.x - bullet.x < (bullet.width + wall.width)/2){
-  bullet.velocityX = 0;
-  
-  var deformation = 0.5 * weight * speed * speed/22500;
-  
-  if(deformation>180){
-    bullet.shapeColor = color(255,0,0);
-  }
-
-  if(deformation < 180 && deformation > 100){
-    bullet.shapeColor = color(230,230,0);
-  }
-
-  if(deformation < 100){
-    bullet.shapeColor = color(0,255,0);
-  }
-}
+  background("skyblue");
+ 
+ 
 
   drawSprites();
 }
+
